91168ecb59c71e443cc871eb25d4ff18c46bc3dd
Ghifari Octaverin <ghifariocta21@gmail.com>
2023-01-11 20:34:28 +0700

    add Entity

---
+++ core/src/main/java/com/gosty/myotakulist/core/data/source/local/entity/common/GeneralEntity.kt
@@ -0,0 +1,8 @@
+package com.gosty.myotakulist.core.data.source.local.entity.common
+
+data class GeneralEntity(
+    var malId: Int,
+    var type: String,
+    var name: String,
+    var url: String
+)
---
+++ core/src/main/java/com/gosty/myotakulist/core/data/source/local/entity/common/TitleEntity.kt
@@ -0,0 +1,8 @@
+package com.gosty.myotakulist.core.data.source.local.entity.common
+
+import com.google.gson.annotations.SerializedName
+
+data class TitleEntity(
+    var type: String,
+    var title: String
+)
