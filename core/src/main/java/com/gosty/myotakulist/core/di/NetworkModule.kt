8726ddd50807438217a59ca14e16112599ff2584
Ghifari Octaverin <ghifariocta21@gmail.com>
2023-02-10 21:43:32 +0700

    add certificate pinning

--- core/src/main/java/com/gosty/myotakulist/core/di/NetworkModule.kt
+++ core/src/main/java/com/gosty/myotakulist/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -22,11 +23,18 @@ class NetworkModule {
         } else {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
+        val hostname = "api.jikan.moe"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/OkKFoHjhk70gFgJbXL4o76+32oMbU9gjXDbyDfHGjLg=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
 
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
